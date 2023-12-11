3aae69f356a2e209d81c665145197955bcb9c16b
naktus-kouki <k-natus@digeon.co>
2023-12-11 15:22:41 +0000

    refactoring

--- index.scala
+++ index.scala
@@ -1,25 +1,25 @@
-object solver {
-  private def solver(N: Int, S: String): String = {
-    val isOdd = if (N % 2 == 0) false else true
-    var head: Char = S.apply(if (isOdd) N - 2 else N - 1)
+object Solver {
+  def solve(N: Int, S: String): String = {
+    val isOdd = N % 2 == 1
+    var head = S(if (isOdd) N - 2 else N - 1)
     var index = 0
+
     S.reverse
       .substring(if (isOdd) 1 else 0)
-      .filter(p => {
-        val filter: Boolean =
-          if (index % 2 == 0) p.compareTo(head) >= 0
-          else p.compareTo(head) < 0
+      .filter { p =>
+        val filter = if (index % 2 == 0) p >= head else p < head
         if (filter) head = p
         index += 1
         filter
-      })
+      }
       .reverse
   }
+
   def main(args: Array[String]): Unit = {
-    assert(solver(2, "aa") == "a", "test1 failed")
-    assert(solver(4, "abcd") == "acd", "test2 failed")
-    assert(solver(10, "aaaaaaaabb") == "aaaab", "test3 failed")
-    assert(solver(6, "xskszy") == "sky", "test4 failed")
+    assert(solve(2, "aa") == "a", "test1 failed")
+    assert(solve(4, "abcd") == "acd", "test2 failed")
+    assert(solve(10, "aaaaaaaabb") == "aaaab", "test3 failed")
+    assert(solve(6, "xskszy") == "sky", "test4 failed")
     println("✅ All tests succeeded")
   }
 }
